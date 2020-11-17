9fb0e22afa8d74624dcb5eb70ba4417d9bac6986
nareh <53708656+nareha@users.noreply.github.com>
2020-11-17 00:50:39 -0800

    Update pong

--- src/commands/fun/PongCommand.js
+++ src/commands/fun/PongCommand.js
@@ -5,16 +5,21 @@ module.exports = class PongCommand extends BaseCommand {
     super('pong', 'fun', []);
   }
 
-  async run(client, message) {
-    const sayEmbed = new Discord.MessageEmbed()
+  async run(message) {
+    const args = message.content.slice(prefix.length).split(/ ;/);
+    const commandName = args.shift().toLowerCase();
+    const sayEmbed;
+    if (commandName === 'ping') {
+      sayEmbed = new Discord.MessageEmbed()
       .setTitle(`pong!`)
       .setColor("#84daf8")
+    }
     try{
       if (message.author.bot) return undefined;
       await message.channel.send(sayEmbed);
     } catch (err) {
       console.log(err);
-      message.channel.send('I am not able to say that message ):')
+      message.channel.send('I couldn\'t pong ):')
     }
   }
 }
