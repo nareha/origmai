2f00ab6de84694ff440b827667efe4561e1eb832
nareh <53708656+nareha@users.noreply.github.com>
2020-12-03 00:03:41 -0800

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -2,6 +2,25 @@ require('dotenv').config();
 const { Client } = require('discord.js');
 const { registerCommands, registerEvents } = require('./utils/registry');
 const client = new Client();
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
 
 (async () => {
   client.commands = new Map();
